54bc11771d0c56376b3f371af9466896f69a4987
Mario Capitbrok <mario@capitbrok.com>
2023-02-23 15:30:12 -0600

    Chapter 11.

---
+++ .history/11_AsynchronousProgramming/01_callbacks_20230221132952.js
@@ -0,0 +1,1 @@
+setTimeout(() => console.logg('Tick'), 500)
---
+++ .history/index_20230222140720.js
@@ -0,0 +1,6 @@
+const data = {
+  data: [{ id: 1, email: 'some_email@email.com', name: 'Some Name' }],
+  duplicates: {
+    'some_email@email.com': ['some_email2@email.com', 'some_emai@email.com'],
+  },
+}
